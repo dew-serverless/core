de51ad6d75e847990a279e901263b1e045822016
Li Zhineng <im@zhineng.li>
2023-11-10 20:43:21 +0800

    register queue event handler

--- stubs/consoleHandler.php
+++ stubs/consoleHandler.php
@@ -4,6 +4,8 @@ use Dew\Core\Cli\CliEvent;
 use Dew\Core\Cli\CliHandler;
 use Dew\Core\EventManager;
 use Dew\Core\FunctionCompute;
+use Dew\Core\Queue\MnsEvent;
+use Dew\Core\Queue\MnsHandler;
 use Dew\Core\RoadRunner;
 use Dew\Core\Scheduler\SchedulerEvent;
 use Dew\Core\Scheduler\SchedulerHandler;
@@ -12,6 +14,7 @@ $events = new EventManager(RoadRunner::createFromGlobal());
 
 $events->register(CliEvent::class, CliHandler::class);
 $events->register(SchedulerEvent::class, SchedulerHandler::class);
+$events->register(MnsEvent::class, MnsHandler::class);
 
 $events->contextUsing(FunctionCompute::createFromEnvironment());
 
